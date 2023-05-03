05b22a12f67fa828e2d40b664d8501568cc34b61
David <datatheyvit123@gmail.com>
2023-05-03 16:42:44 -0500

    Generando archivo cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'cliente-add-edit',
+    loadChildren: () => import('./cliente/cliente-add-edit/cliente-add-edit.module').then( m => m.ClienteAddEditPageModule)
   },
+
 ];
 
 @NgModule({
