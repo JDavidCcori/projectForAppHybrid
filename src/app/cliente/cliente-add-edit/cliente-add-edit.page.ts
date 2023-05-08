b062cd7ffe2157e34492a7a68987981462e7528a
David <datatheyvit123@gmail.com>
2023-05-08 18:03:31 -0500

    Modificación incompleta

--- src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
+++ src/app/cliente/cliente-add-edit/cliente-add-edit.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-cliente-add-edit',
@@ -6,10 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cliente-add-edit.page.scss'],
 })
 export class ClienteAddEditPage implements OnInit {
+  
+  clienteForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
-  constructor() { }
+  nombre: string = "David";
+  cliente = {
+    nombre: "David",
+    email: "dccorialfa@gmail.com",
+  }
 
   ngOnInit() {
+    this.clienteForm = this.formBuilder.group(
+      {
+        nombre: [
+          '', 
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(50),
+          ])
+        ],
+        email: [
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.email,
+          ])
+        ],
+        telefono: [
+          '',
+          Validators.compose([
+            Validators.required,
+          ])
+        ],
+        ingreso: [
+          '0',
+          Validators.compose([
+            Validators.required,
+            Validators.min(0),
+          ])
+        ],
+        nacimiento: [
+          '',
+          Validators.required,
+        ]
+      }
+    );
+  }
+
+  submit() {
+    console.log(this.nombre);
+    this.nombre = "Other";
   }
 
 }
